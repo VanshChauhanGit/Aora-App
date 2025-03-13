af862bcadebc7285df7f8c7a63953b2a8d3e868d
Vanshchauhan-1106 <Vanshchauhan1106@gmail.com>
2025-03-13 21:26:36 +0530

    Add loading indicator and toast notifications to video upload process; refactor create post UI

--- app/(tabs)/create.jsx
+++ app/(tabs)/create.jsx
@@ -16,10 +16,11 @@ import * as DocumentPicker from "expo-document-picker";
 import { router } from "expo-router";
 import { createPost } from "@/lib/appwrite";
 import { useGlobalContext } from "@/context/GlobalProvider";
+import { useToast } from "react-native-toast-notifications";
+import Loader from "@/components/Loader";
 
 const Create = () => {
   const [uploading, setUploading] = useState(false);
-  const [videoUrl, setVideoUrl] = useState("");
   const [form, setForm] = useState({
     title: "",
     video: null,
@@ -27,6 +28,7 @@ const Create = () => {
     prompt: "",
   });
   const { user } = useGlobalContext();
+  const toast = useToast();
 
   const player = useVideoPlayer(form?.video?.uri, (player) => {
     player.loop = true;
@@ -61,8 +63,9 @@ const Create = () => {
 
     try {
       await createPost({ ...form, userId: user.$id });
-
-      Alert.alert("Success", "Post uploaded successfully!");
+      toast.show("Post uploaded successfully!", {
+        type: "success",
+      });
       router.push("/home");
     } catch (error) {
       Alert.alert("Error", error.message);
@@ -78,89 +81,97 @@ const Create = () => {
   };
 
   return (
-    <SafeAreaView className="h-full bg-primary">
-      <ScrollView className="px-4 mt-5">
-        <Text className="text-2xl text-white font-psemibold">Upload Video</Text>
-        <FormField
-          title="Video Title"
-          value={form.title}
-          placeholder="Give your video a catchy title!"
-          handleChangeText={(e) => setForm({ ...form, title: e })}
-          otherStyles={"mt-10"}
-        />
+    <>
+      <Loader visible={uploading} />
 
-        <View className="w-full gap-2 mt-7">
-          <Text className="text-base text-gray-100 font-pmedium">
+      <SafeAreaView className="h-full bg-primary">
+        <ScrollView className="px-4 mt-5">
+          <Text className="text-2xl text-white font-psemibold">
             Upload Video
           </Text>
+          <FormField
+            title="Video Title"
+            value={form.title}
+            placeholder="Give your video a catchy title!"
+            handleChangeText={(e) => setForm({ ...form, title: e })}
+            otherStyles={"mt-10"}
+            disable={uploading}
+          />
 
-          <TouchableOpacity onPress={() => openPicker("video")}>
-            {form.video ? (
-              <View className="items-center justify-center w-full mb-3 border h-60 border-secondary/20 rounded-xl">
-                <VideoView
-                  player={player}
-                  style={{ width: "100%", height: "100%", borderRadius: 10 }}
-                  nativeControls={false}
-                  contentFit="cover"
-                />
-              </View>
-            ) : (
-              <View className="items-center justify-center w-full h-40 px-4 bg-black-100 rounded-2xl">
-                <View className="items-center justify-center border border-dashed size-14 border-secondary-100">
-                  <Image
-                    source={icons.upload}
-                    className="w-1/2 h-1/2 "
-                    resizeMode="contain"
+          <View className="w-full gap-2 mt-7">
+            <Text className="text-base text-gray-100 font-pmedium">
+              Upload Video
+            </Text>
+
+            <TouchableOpacity onPress={() => openPicker("video")}>
+              {form.video ? (
+                <View className="items-center justify-center w-full mb-3 border h-60 border-secondary/20 rounded-xl">
+                  <VideoView
+                    player={player}
+                    style={{ width: "100%", height: "100%", borderRadius: 10 }}
+                    nativeControls={false}
+                    contentFit="cover"
                   />
                 </View>
-              </View>
-            )}
-          </TouchableOpacity>
-        </View>
+              ) : (
+                <View className="items-center justify-center w-full h-40 px-4 bg-black-100 rounded-2xl">
+                  <View className="items-center justify-center border border-dashed size-14 border-secondary-100">
+                    <Image
+                      source={icons.upload}
+                      className="w-1/2 h-1/2 "
+                      resizeMode="contain"
+                    />
+                  </View>
+                </View>
+              )}
+            </TouchableOpacity>
+          </View>
 
-        <View className="gap-2 mt-7">
-          <Text className="text-base text-gray-100 font-pmedium">
-            Thumbnail Image
-          </Text>
+          <View className="gap-2 mt-7">
+            <Text className="text-base text-gray-100 font-pmedium">
+              Thumbnail Image
+            </Text>
 
-          <TouchableOpacity onPress={() => openPicker("image")}>
-            {form.thumbnail ? (
-              <Image
-                source={{ uri: form.thumbnail.uri }}
-                className="w-full h-60 rounded-2xl"
-                resizeMode="cover"
-              />
-            ) : (
-              <View className="flex-row items-center justify-center w-full h-16 gap-2 px-4 border-2 bg-black-100 rounded-2xl border-black-200">
+            <TouchableOpacity onPress={() => openPicker("image")}>
+              {form.thumbnail ? (
                 <Image
-                  source={icons.upload}
-                  className="size-7"
-                  resizeMode="contain"
+                  source={{ uri: form.thumbnail.uri }}
+                  className="w-full h-60 rounded-2xl"
+                  resizeMode="cover"
                 />
-                <Text className="text-sm text-gray-100 font-pmedium">
-                  Choose a file
-                </Text>
-              </View>
-            )}
-          </TouchableOpacity>
-        </View>
+              ) : (
+                <View className="flex-row items-center justify-center w-full h-16 gap-2 px-4 border-2 bg-black-100 rounded-2xl border-black-200">
+                  <Image
+                    source={icons.upload}
+                    className="size-7"
+                    resizeMode="contain"
+                  />
+                  <Text className="text-sm text-gray-100 font-pmedium">
+                    Choose a file
+                  </Text>
+                </View>
+              )}
+            </TouchableOpacity>
+          </View>
 
-        <FormField
-          title="AI Prompt"
-          value={form.prompt}
-          placeholder="The prompt you used to create this video"
-          handleChangeText={(e) => setForm({ ...form, prompt: e })}
-          otherStyles={"mt-7"}
-        />
+          <FormField
+            title="AI Prompt"
+            value={form.prompt}
+            placeholder="The prompt you used to create this video"
+            handleChangeText={(e) => setForm({ ...form, prompt: e })}
+            otherStyles={"mt-7"}
+            disable={uploading}
+          />
 
-        <CustomButton
-          title="Upload Video"
-          containerStyles={"my-7"}
-          isLoading={uploading}
-          handlePress={submit}
-        />
-      </ScrollView>
-    </SafeAreaView>
+          <CustomButton
+            title="Upload Video"
+            containerStyles={"my-7"}
+            isLoading={uploading}
+            handlePress={submit}
+          />
+        </ScrollView>
+      </SafeAreaView>
+    </>
   );
 };
 
